12c7d1efe1c1390fccc8d2b73ada76d5a733027c
GabrielaMendozal <mendozalizethg@gmail.com>
2022-04-07 10:06:36 -0500

    API onclick useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import Pokemon from './Componentes/Pokemon/Pokemon';
 
 function App() {
+  const [pokemons, setPokemons] = useState( []);
+  const [URL, setURL] = useState( "https://pokeapi.co/api/v2/pokemon/?limit=807")
+/*
+  useEffect(() =>{
+    let configuracion ={
+      method : "GET"
+    };
+    fetch (URL, configuracion)
+      .then( respuesta => {
+        if( respuesta.ok){
+          return respuesta.json();
+        }
+      })
+      .then( listaPokemons =>{
+        console.log(listaPokemons);
+        if( listaPokemons.results){
+          setPokemons( (pokemonPrev) => listaPokemons.results );
+        }  
+      });
+  }, []);*/
+
+  const buscar = (event) => {
+    event.preventDefault();
+    let configuracion ={
+      method : "GET"
+    };
+    fetch (URL, configuracion)
+      .then( respuesta => {
+        if( respuesta.ok){
+          return respuesta.json();
+        }
+      })
+      .then( listaPokemons =>{
+        console.log(listaPokemons);
+        if( listaPokemons.results){
+          setPokemons( (pokemonPrev) => listaPokemons.results );
+        }  
+      });
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+          <button type="submit"  onClick={buscar}>
+            Fetch Pokemon
+          </button>
+     
+      <div className='pokemons'>
+        {
+          pokemons.map (( pokemons, indice) =>{
+            return ( <Pokemon key={'pokemons_' + indice} pokemons={pokemons}/>)
+          }
+          )
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Componentes/Pokemon/Pokemon.js
@@ -0,0 +1,13 @@
+function Pokemon (props){
+
+    return(
+        <div>
+            <p>
+                {props.pokemons.name}
+            </p>
+            
+        </div>
+    )
+}
+
+export default Pokemon;
